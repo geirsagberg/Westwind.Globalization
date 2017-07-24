fa47f89ad5fbfb387cbf52f60a8a4340fb75d7a4
Geir Sagberg <geir.sagberg@gmail.com>
2017-07-24 10:26:34 +0200

    Create NuGet pack script and packages

---
+++ pack.sh
@@ -0,0 +1,2 @@
+dotnet pack Westwind.Globalization.Core -o ../Nuget/Westwind.Globalization.Core -c Release
+dotnet pack Westwind.Globalization.Core.Web -o ../Nuget/Westwind.Globalization.Core.Web -c Release
